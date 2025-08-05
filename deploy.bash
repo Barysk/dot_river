a11687906c6cf02a8e6abc0ecb02303265039e80
Boris Komarov <boris.komarov@proton.me>
2025-08-05 13:06:20 +0200

    deploy script changed

--- deploy.bash
+++ deploy.bash
@@ -2,20 +2,20 @@
 
 echo "Deploying dot_river configs"
 
-sudo cp ./dot_bash_profile $HOME/.bash_profile
+cp -rf ./dot_bash_profile $HOME/.bash_profile
 
-sudo cp ./dot_bashrc $HOME/.bashrc
+cp -rf ./dot_bashrc $HOME/.bashrc
 
 if [ ! -d "$HOME/.fonts" ]; then
 	mkdir -p $HOME/.fonts
 fi
 
-sudo cp -r ./dot_fonts/* $HOME/.fonts/
+cp -rf ./dot_fonts/* $HOME/.fonts/
 
 if [ ! -d "$HOME/.config" ]; then
 	mkdir -p $HOME/.config
 fi
 
-sudo cp -r ./dot_config/* $HOME/.config/
+cp -rf ./dot_config/* $HOME/.config/
 
 echo "Done."
